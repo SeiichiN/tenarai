f32cdac4ebff80872c3234d7e6a69573176cfb80
SeiichiN <billie175@gmail.com>
2016-09-04 18:43:40 +0900

    tenarai.scm updated.

--- tenarai.scm
+++ tenarai.scm
@@ -1,15 +1,17 @@
-;;; Scheme手習い
-;;; p16
-(defun lat? (l)
-           (cond
-             ((null l) t)
-             ((atom (car l))(lat? (cdr l)))
-             (t nil)))
+;; Scheme手習い
+;; p16
+(define lat?
+          (lambda (l)
+            (cond
+              ((null? l) #t)
+              ((atom? (car l))(lat? (cdr l)))
+              (else #f))))
 
-;;; p23
-(defun member? (a lat)
-           (cond
-             ((null lat) nil)
-             (t (or (eq (car lat) a)
-                    (member? a (cdr lat))))))
+;; p23
+(define member?
+          (lambda (a lat)
+            (cond
+              ((null? lat) nil)
+              (else (or (eq? (car lat) a)
+                        (member? a (cdr lat)))))))
 
