f30ff98bce44b2c6d9888d2f85174048b1a99a66
SeiichiN <billie175@gmail.com>
2016-09-04 21:38:41 +0900

    teranai.scm updated at 0904.

--- tenarai.scm
+++ tenarai.scm
@@ -15,3 +15,34 @@
               (else (or (eq? (car lat) a)
                         (member? a (cdr lat)))))))
 
+;; p35
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat)(quote ()))
+     (else (cond
+            ((eq? (car lat) a)(cdr lat))
+            (else (cons (car lat)
+                        (rember a (cdr lat)))))))))
+
+;; p46
+(define firsts
+  (lambda (l)
+    (cond
+     ((null? l) (quote()) )
+     (else (cons (car (car l))
+                 (firsts (cdr l)))))))
+
+;; p52
+(define insertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat)(quote ()))
+     (else
+      (cond
+       ((eq? (car lat) old)
+        (cons old (cons new (cdr lat))))
+       (else (cons (car lat)
+                   (insertR new old (cdr lat)))))))))
+
+
