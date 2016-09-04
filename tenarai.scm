3ff8ed2c9942b039d2f09b17759eac8a83b170e0
SeiichiN <billie175@gmail.com>
2016-09-05 07:50:11 +0900

    tenarai.scm updated at 0905-07:49

--- tenarai.scm
+++ tenarai.scm
@@ -45,4 +45,78 @@
        (else (cons (car lat)
                    (insertR new old (cdr lat)))))))))
 
+;; 52
+(define insertL
+          (lambda (new old lat)
+            (cond
+              ((null? lat)(quote()))
+              (else (cond
+                      ((eq? (car lat) old)
+                       (cons new lat))
+                      (else (cons (car lat)
+                                  (insertL new old (cdr lat)))))))))
 
+;; 53
+(define subst
+          (lambda (new old lat)
+            (cond
+              ((null? lat)(quote()))
+              (else (cond
+                      ((eq? (car lat) old)
+                       (cons new (cdr lat)))
+                      (else (cons (car lat)
+                                  (subst new old (cdr lat)))))))))
+
+;; 53
+(define subst2
+          (lambda (new o1 o2 lat)
+            (cond
+              ((null? lat)(quote()))
+              (else (cond
+                      ((or (eq? (car lat) o1)(eq? (car lat) o2))
+                       (cons new (cdr lat)))
+                      (else (cons (car lat)
+                                  (subst2 new old (cdr lat)))))))))
+
+;; p54
+(define multirember
+  (lambda (a lat)
+    (cond
+     ((null? lat)(quote ()))
+     (else (cond
+            ((eq? (car lat) a)(multirember a (cdr lat)))
+            (else (cons (car lat) (multirember a (cdr lat)))))))))
+
+;; p57
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat)(quote ()))
+     (else
+      (cond
+       ((eq? (car lat) old)
+        (cons old (cons new (multiinsertR new old (cdr lat)))))
+       (else (cons (car lat)
+                   (multiinsertR new old (cdr lat)))))))))
+
+;; 58
+(define multiinsertL
+          (lambda (new old lat)
+            (cond
+              ((null? lat)(quote()))
+              (else (cond
+                      ((eq? (car lat) old)
+                       (cons new (cons old (multiinsertL new old (cdr lat)))))
+                      (else (cons (car lat)
+                                  (multiinsertL new old (cdr lat)))))))))
+
+;; 59
+(define multisubst
+          (lambda (new old lat)
+            (cond
+              ((null? lat)(quote()))
+              (else (cond
+                      ((eq? (car lat) old)
+                       (cons new (multisubst new old (cdr lat))))
+                      (else (cons (car lat)
+                                  (multisubst new old (cdr lat)))))))))
