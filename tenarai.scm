52a42c154d657ebaa15796002346a391bb2a29a6
SeiichiN <billie175@gmail.com>
2016-09-10 10:35:13 +0900

    tenarai.scm at 0910-10:35

--- tenarai.scm
+++ tenarai.scm
@@ -340,3 +340,19 @@
       (o+ (occur* a (car l))
           (occur* a (cdr l)))))))
 
+;; p87
+;; リストlの中のoldをnewに置き換える
+(define subst*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? old (car l))
+        (cons new (subst* new old (cdr l))))
+       (else
+        (cons (car l) (subst* new old (cdr l))))))
+     (else
+      (cons (subst* new old (car l))
+            (subst* new old (cdr l)))))))
+
